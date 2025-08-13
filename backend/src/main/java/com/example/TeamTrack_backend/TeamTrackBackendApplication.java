a9936928e1c320885160280506430a5dca00231f
Amr Ibrahim <amr.ali.ibrahim247@gmail.com>
2025-08-13 13:03:09 -0700

    Added Spring boot to backend with gradle

---
+++ backend/src/main/java/com/example/TeamTrack_backend/TeamTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeamTrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTrackBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/TeamTrack_backend/TeamTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeamTrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
